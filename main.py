261758e1a1058bc987d97d14dbe9573e524a2a48
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-08-12 22:25:26 +0530

    Handled Game Over conditions
    Added game over conditions: Snake hitting the screen boundary and Snake eating itself results in Game Over.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
     snakeY = 350
     snakeSize = 25
     head = []
-    snakeLength = 1
+    snakeLength = 25
     snakeList = []
     fps = 30
     init_Velocity = 6
@@ -40,59 +40,73 @@ def main():
 
     clock = p.time.Clock()
     #Game loop
-    while not exit_game:           
-        for event in p.event.get():
-            if event.type == p.QUIT:
-                exit_game = True
-            
-            elif event.type == p.KEYDOWN:
+    while not exit_game:
+        if game_over:
+            display_text(screen, "Game Over!", 503, 353, "white", 65)
+            display_text(screen, "Game Over!", 500, 350, "black", 65)
+            for event in p.event.get():
+                if event.type == p.QUIT:
+                    exit_game = True
+
+        else:
+            for event in p.event.get():
+                if event.type == p.QUIT:
+                    exit_game = True
+                        
+                elif event.type == p.KEYDOWN:
                     if event.key == p.K_RIGHT:
-                         velocityX = init_Velocity
-                         velocityY = 0
+                        velocityX = init_Velocity
+                        velocityY = 0
 
                     elif event.key == p.K_LEFT:
-                         velocityX = -init_Velocity
-                         velocityY = 0
+                        velocityX = -init_Velocity
+                        velocityY = 0
 
                     elif event.key == p.K_UP:
-                         velocityX = 0
-                         velocityY = -init_Velocity
+                        velocityX = 0
+                        velocityY = -init_Velocity
 
                     elif event.key == p.K_DOWN:
-                         velocityX = 0
-                         velocityY = init_Velocity
+                        velocityX = 0
+                        velocityY = init_Velocity
 
-        screen.fill("green")
-        display_text(screen, "Score: " + str(score), 11, 11, "black", 55)
-        display_text(screen, "Score: " + str(score), 10, 10, "yellow", 55)
+            screen.fill("green")
 
-        #Drawing the Fruit
-        p.draw.rect(screen, "red", [fruitX, fruitY, fruitSize, fruitSize])
+            #Displaying the score
+            display_text(screen, "Score: " + str(score), 11, 11, "black", 55)
+            display_text(screen, "Score: " + str(score), 10, 10, "yellow", 55)
 
-        #Drawing the snake
-        head = [snakeX, snakeY]
-        snakeList.append(head)
+            #Drawing the Fruit
+            p.draw.rect(screen, "red", [fruitX, fruitY, fruitSize, fruitSize])
 
-        if(len(snakeList) > snakeLength):
-             del(snakeList[0])
+            #Handling game over
+            if([snakeX, snakeY] in snakeList or snakeX < 0 or snakeX + snakeSize > WIDTH or snakeY < 0 or snakeY + snakeSize > HEIGHT):
+                game_over = True
 
-        draw_snake(screen, snakeList, "black", snakeSize)
+            #Drawing the snake
+            head = [snakeX, snakeY]
+            snakeList.append(head)
+
+            if(len(snakeList) > snakeLength):
+                del(snakeList[0])
+
+            draw_snake(screen, snakeList, "black", snakeSize)
+
+            #Snake movement
+            snakeX += velocityX
+            snakeY += velocityY
+
+            #Spawning fruit after eating
+            if(abs(snakeX - fruitX) <= 5 and abs(fruitY - snakeY) <= 5):
+                fruitX = random.randint(0, WIDTH-fruitSize)
+                fruitY = random.randint(70, HEIGHT-fruitSize)
+                score += 1
+                snakeLength += 1
+                if(score % 10 == 0):
+                    init_Velocity += 1
         
         p.display.update()
         clock.tick(fps)
-
-        #snake movement
-        snakeX += velocityX
-        snakeY += velocityY
-
-        #spawning fruit after eating
-        if(abs(snakeX - fruitX) <= 5 and abs(fruitY - snakeY) <= 5):
-            fruitX = random.randint(0, WIDTH-fruitSize)
-            fruitY = random.randint(70, HEIGHT-fruitSize)
-            score += 1
-            snakeLength += 1
-            if(score % 10 == 0):
-                 init_Velocity += 1
 
     p.quit()
 
